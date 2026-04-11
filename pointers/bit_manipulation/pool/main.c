33219198fd7e83d81c1f05f1f63f8b80f261533d
Giulio Lasta <giuliolasta@gmail.com>
2026-04-11 11:48:07 +0200

    Bit manipulation POOLS excerices

--- pointers/bit_manipulation/pool/main.c
+++ pointers/bit_manipulation/pool/main.c
@@ -1,6 +1,41 @@
 #include "pool.c"
 
 int main()
-{
+{   
+    Pool_t pool;
 
-}
+    pool_init(&pool);
+
+    Pool_t *pool1 = pool_alloc(&pool);
+    Pool_t *pool2 = pool_alloc(&pool);
+    Pool_t *pool3 = pool_alloc(&pool);
+    Pool_t *pool4 = pool_alloc(&pool);
+    Pool_t *pool5 = pool_alloc(&pool);
+    Pool_t *pool6 = pool_alloc(&pool);
+    Pool_t *pool7 = pool_alloc(&pool);
+    Pool_t *pool8 = pool_alloc(&pool);
+    Pool_t *pool9 = pool_alloc(&pool);
+    Pool_t *pool10 = pool_alloc(&pool);
+    Pool_t *pool11 = pool_alloc(&pool);
+    Pool_t *pool12 = pool_alloc(&pool);
+    Pool_t *pool13 = pool_alloc(&pool);
+    Pool_t *pool14 = pool_alloc(&pool);
+    Pool_t *pool15 = pool_alloc(&pool);
+
+    Pool_t *tmp1 = pool_alloc(&pool);
+    printf("%p\n", tmp1);
+
+    Pool_t *tmp2 = pool_alloc(&pool);
+    printf("%p\n", tmp2);
+
+
+    printf("free count before free: %d\n", pool_free_count(&pool));
+
+    printf("Pointer 11 before free: %p\n", pool11);
+    pool_free(&pool, pool11);
+    printf("Pointer 11 after free: %p\n", pool11);
+    pool11 = NULL;
+    
+    printf("free count dopo free: %d\n", pool_free_count(&pool));
+
+}   
--- pointers/bit_manipulation/pool/pool.c
+++ pointers/bit_manipulation/pool/pool.c
@@ -10,16 +10,49 @@ void  pool_init (Pool_t *pool)
         node->next = tmp;
         tmp = node;
     }
+    pool->free_count = POOL_BLOCKS;
 
     pool->free_head = tmp;
 }
 
+void *pool_alloc(Pool_t *pool)
+{
+    FreeNode_t *tmp = pool->free_head;
+
+    if(tmp == NULL)
+    {
+        return NULL;
+    }
+
+    pool->free_head = tmp->next;
+    pool->free_count--;
+
+    return tmp;
+}
+
+void  pool_free (Pool_t *pool, void *ptr)
+{
+    FreeNode_t *tmp = pool->free_head;
+    pool->free_head = ptr;
+    pool->free_head->next = tmp;
+    pool->free_count++;
+}
+
+int   pool_free_count(Pool_t *pool)
+{
+    return pool->free_count;
+}
+
 /*
 
-* typedef struct {
-    uint8_t     buf[BLOCK_SIZE * POOL_BLOCKS];
-    FreeNode_t *free_head;
-    size_t      free_count;
+typedef struct FreeNode {
+    struct FreeNode *next;
+} FreeNode_t;
+
+typedef struct {
+    uint8_t     buf[BLOCK_SIZE * POOL_BLOCKS]; // 32 * 16 * 8
+    FreeNode_t *free_head; // 16
+    size_t      free_count; // 32
 } Pool_t;
 
  */
